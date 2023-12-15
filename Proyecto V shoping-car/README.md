185c589072b748bae71f772c28c7839038986fbc
eduardof5sg <149663359+eduardof5sg@users.noreply.github.com>
2023-12-15 15:26:09 +0100

    primer commit

---
+++ Proyecto V shoping-car/README.md
@@ -0,0 +1,53 @@
+# e-commerce-frontend
+This repository hosts the development project of an e-commerce website, carried out by a group of web developers. The main goal of this project is to create an attractive and functional e-commerce website using HTML and CSS web technologies. The site's design has been crafted in Figma, following accessibility and UX/UI principles to ensure an optimal user experience. The project also includes adapted versions for both mobile and desktop devices, ensuring accessibility across all platforms.
+
+Project Requirements:
+
+1. Implementation of the design using HTML and CSS.
+2. Creation of a section displaying all products available in the catalog.
+3. Integration of registration, login, and contact forms to enhance user interaction.
+4. Use of semantic HTML to improve site structure and accessibility.
+5. Fully responsive design, with a "mobile-first" approach for an optimal experience on mobile devices.
+6. Incorporation of CSS animations to enhance aesthetics and user experience.
+7. Use of copyright-free images from image banks like "Pixabay" to maintain project legality and visual quality.
+
+Additional Features:
+
+1. Implementation of JavaScript for functionality to hide or show the shopping cart, improving site usability.
+2. Configuration of deployment options to ensure online availability of the website.
+3. Linkage with the Figma design project and access to the source code through GitHub.
+
+This repository serves as a comprehensive record of the project, providing access to the source code, documentation, and resources associated with the development of an e-commerce website with a focus on accessibility, visual quality, and functionality.
+
+Figma Prototype: [Link to Figma](https://www.figma.com/file/wk1QjeIDZeqEO6HEbloBUd/Proyect-2?type=design&node-id=24%3A357&mode=design&t=kecD3cQriZEdUVcY-1)
+
+Trello Board: [Link to Trello](https://trello.com/invite/b/sXs8fjfw/ATTI7167a31b9172742f6a91ae8a71b73823EFA37015/e-comerce-grupo-2)
+
+
+# e-commerce-frontend
+Español/Spanish:
+
+Este repositorio alberga el proyecto de desarrollo de un sitio web de comercio electrónico, realizado por un grupo de desarrolladores web. El objetivo principal de este proyecto es crear un sitio web de comercio electrónico atractivo y funcional utilizando las tecnologías web HTML y CSS. El diseño del sitio se ha elaborado en Figma, siguiendo principios de accesibilidad y UX/UI para garantizar una experiencia de usuario óptima. El proyecto también incluye versiones adaptadas para dispositivos móviles y de escritorio, asegurando la accesibilidad en todas las plataformas.
+
+Requisitos del Proyecto:
+
+Implementación del diseño utilizando HTML y CSS.
+Creación de una sección que muestre todos los productos disponibles en el catálogo.
+Integración de formularios de registro, inicio de sesión y contacto para mejorar la interacción con los usuarios.
+Uso de HTML semántico para mejorar la estructura y accesibilidad del sitio.
+Diseño totalmente responsivo, con un enfoque "mobile-first" para brindar una experiencia óptima en dispositivos móviles.
+Incorporación de animaciones CSS para mejorar la estética y la experiencia del usuario.
+Utilización de imágenes sin derechos de autor de bancos de imágenes, como "Pixabay", para mantener la legalidad y la calidad visual del proyecto.
+Funcionalidades Adicionales:
+
+Implementación de JavaScript para la funcionalidad de ocultar o mostrar el carrito de compras, mejorando la usabilidad del sitio.
+Configuración de opciones de deployment para asegurar la disponibilidad en línea del sitio web.
+Vinculación con el proyecto de diseño en Figma y acceso al código fuente a través de GitHub.
+
+Este repositorio sirve como un registro completo del proyecto y proporciona acceso al código fuente, documentación y recursos asociados al desarrollo de un sitio web de comercio electrónico con un enfoque en la accesibilidad, la calidad visual y la funcionalidad.
+
+Prototipo en Figma:
+https://www.figma.com/file/wk1QjeIDZeqEO6HEbloBUd/Proyect-2?type=design&node-id=24%3A357&mode=design&t=kecD3cQriZEdUVcY-1
+
+Trello:
+https://trello.com/invite/b/sXs8fjfw/ATTI7167a31b9172742f6a91ae8a71b73823EFA37015/e-comerce-grupo-2
---
+++ Proyecto V shoping-car/scripts/formulario.js
@@ -0,0 +1,161 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+   const form = document.getElementById("formulario1")
+   const formReg = document.getElementById("formulario2")
+   
+   // variables para el login //
+   const correo1= document.getElementById("correo")
+   const passUno= document.getElementById("pass1")
+
+   //Variables para el Regitro //
+   
+   const nombre = document.getElementById("nombre")
+   const apellidos = document.getElementById("apellidos")
+   const telefono = document.getElementById("telefono")
+   const correo2 = document.getElementById("correo2")
+   const confirmCorreo = document.getElementById("confirmarCorreo")
+   const pass = document.getElementById("pass2")
+   const confirmPass = document.getElementById("confirmPass")
+
+   form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    validaCampos()
+   });
+
+   formReg.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    validaCampos2()
+   });
+
+     const validaCampos = () => {
+
+        const correo1Valor= correo1.value.trim();               
+        const passUnoValor= passUno.value.trim();
+        const correoValido = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+       
+
+        if(!correo1Valor) {
+            validaFallo(correo1, 'introduce un Email')
+        } else if (!correoValido.test(correo1Valor)) {
+            validaFallo(correo1, 'Email no válido');        
+        }else {
+            validaOk(correo1, '')
+            
+        }
+
+        if(!passUnoValor) {
+            validaFallo(passUno, 'introduce tu contraseña')
+        }else {
+            validaOk(passUno, 'el usuario no existe')
+        }      
+        
+
+     }
+    
+      const validaFallo = (input, msje) => {
+            const formControl = input.parentElement
+            const aviso = formControl.querySelector('p')   
+            aviso.innerText = msje   
+
+            formControl.className = 'form-control falla'
+        }
+
+      const validaOk = (input, msje) => {
+            const formControl = input.parentElement
+            const aviso = formControl.querySelector('p');
+            aviso.innerText = msje;
+            formControl.className = 'form-control ok'
+      }
+
+      const validaCampos2= () => {
+        
+        let validacionOk = true;
+        const valorNombre = nombre.value.trim();
+        const valorApellidos = apellidos.value;
+        const valorTelefono = telefono.value;
+        const expresionTelefono = /^\d{9}$/;
+        const valorCorreo2 = correo2.value;
+        const correoValido2 = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const valorConfirmCorreo = confirmCorreo.value;
+        const valorPass = pass.value.trim();
+        const expresionPassword = /^(?=.*[A-Z])(?=.*\d).{6,}$/;
+        const valorConfirmPass = confirmPass.value.trim();
+        const checkBox = document.getElementById('formRegistroCheckbox');
+        const labelCheckbox = document.querySelector('label[for="formRegistroCheckbox"]');
+
+
+        if(!valorNombre) {
+            validaFallo(nombre, 'Introduce un nombre')
+            validacionOk=false;
+        }else{
+            validaOk(nombre, '')
+        }
+
+        if(!valorApellidos) {
+            validaFallo(apellidos,'Introduce tus apellidos')
+            validacionOk=false;
+        }else {
+            validaOk(apellidos, '')
+        }
+
+        if(!valorTelefono){
+            validaFallo(telefono, 'Introduce un numero de telefono')
+            validacionOk=false;
+        }else if (!expresionTelefono.test(valorTelefono)){
+            validaFallo(telefono, 'formato de telefono no valido')
+            validacionOk=false;
+        }else {
+            validaOk(telefono,'')
+        }
+
+        if(!valorCorreo2){
+            validaFallo(correo2, 'introduce un email')
+            validacionOk=false;
+        }else if (!correoValido2.test(valorCorreo2)){
+            validaFallo(correo2, 'email no valido')
+            validacionOk=false;
+        }else {
+            validaOk(correo2 , '')
+        }
+
+        if (valorCorreo2 !== valorConfirmCorreo) {
+            validaFallo(confirmCorreo, 'El email no coincide');
+            validacionOk=false;
+        } else {
+            validaOk(confirmCorreo, '');
+            
+        }
+        
+
+        if (!valorPass) {
+            validaFallo(pass, 'porfavor introduce una contraseña')
+            validacionOk=false;
+        }else if (!expresionPassword.test(valorPass)){
+            validaFallo(pass, 'Incluye almenos  una mayuscula y un numero')
+            validacionOk=false;
+        }else {
+            validaOk(pass, '')
+        }
+
+        if (valorPass !== valorConfirmPass) {
+            validaFallo(confirmPass, 'la contraseña no coincide')
+            validacionOk=false;
+        }else {
+            validaOk(confirmPass, '')
+
+        
+        }
+        if (!checkBox.checked) {
+            labelCheckbox.style.color = 'red';
+            validacionOk=false;
+            
+        } else {
+            labelCheckbox.style.color = '';
+        }
+
+        if (validacionOk){
+            window.location.href= 'http://127.0.0.1:3000/index.html'
+        }
+
+      }
+
+})
